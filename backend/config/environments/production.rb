3648995544c37d4cc67b6c7cadbb516a4852ddd5
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-01-09 23:28:51 +0800

    Heroku updates

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Allow front end to make requests
   # config.allowed_cors_origins = ["http://localhost:4000"]
   config.hosts << "my-cvwo-assignment.herokuapp.com"
+  config.assets.complie = true
+  config.serve_static_assests = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
