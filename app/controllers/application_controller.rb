439c821e7be010bc3bfeab4c0e40653dbe798f91
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-01-10 12:28:55 +0800

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    include ActionController::MimeResponds
+
     def fallback_index_html
-        render :file => 'public/index.html'
+        respond_to do |format|
+            format.html { render body: Rails.root.join('public/index.html').read }
+        end
     end
 end
