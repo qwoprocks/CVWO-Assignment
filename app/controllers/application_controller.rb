900613b62e0da8269399e9f1dd498430da313e7d
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2020-01-15 17:11:39 +0800

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::API
 
     def fallback_index_html
         respond_to do |format|
-            format.html { render body: Rails.root.join('build/index.html').read }
+            format.html { render body: Rails.root.join('public/index.html').read }
         end
     end
 end
